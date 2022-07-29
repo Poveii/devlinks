ea332fadb4b1dd9c80d002904ef75c914135b813
Poveii <yellowtutors@gmail.com>
2022-07-29 07:47:58 -0300

    Added a dark detail to avatar image

--- main.js
+++ main.js
@@ -4,17 +4,20 @@ const buttonToggleTheme = document.querySelector("button");
 const iconButton = document.querySelector("button i");
 
 const buttonChangeColor = document.querySelector(".switch-colors");
+const avatarImage = document.querySelector(".avatar");
 
 buttonToggleTheme.addEventListener("click", function () {
   if (buttonToggleTheme.className == "dark-mode") {
     document.body.className = "dark-" + document.body.className;
 
+    avatarImage.classList.add("dark-detail");
     iconButton.className = "ph-sun-dim-fill";
     buttonToggleTheme.className = "light-mode";
   } else {
     const withoutDark = document.body.className.slice(5);
 
     document.body.className = withoutDark;
+    avatarImage.classList.remove("dark-detail");
     iconButton.className = "ph-moon-stars-fill";
     buttonToggleTheme.className = "dark-mode";
   }
