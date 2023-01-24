f9cbee8faed4ccd77460727d407872890dcadc95
Poveii <yellowtutors@gmail.com>
2023-01-24 11:35:46 -0300

    :sparkles: feat: Added my Avatar Photo on App
    - Added avatar.png into assets folder
    - Imported Avatar from avatar.png
    - Added alt to avatar img

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Avatar from "./assets/avatar.png";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div id="profile">
+        <img
+          src={Avatar}
+          alt="Foto do Pablo Gabriel olhando pra câmera de camisa branca sorrindo sem mostrar os dentes, com um fundo azul por trás"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
