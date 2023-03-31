08202219b8b71134a61dc7b08bb98128702f30fa
Poveii <yellowtutors@gmail.com>
2023-03-31 19:41:34 -0300

    feat: ESLint Config and Rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
+  ],
+  "overrides": [],
+  "globals": {
+    window: true,
+    module: true
+  },
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": ["react"],
+  "rules": {
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "double"],
+    "semi": ["error", "never"]
+  }
+}
